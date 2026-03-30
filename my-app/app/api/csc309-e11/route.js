fe986b420bff0487546ffd479d40c90b488f7d73
adamhadzikadunic <adam.hadzikadunic@gmail.com>
2026-03-30 14:42:12 -0400

    testing 2

--- my-app/app/api/csc309-e11/route.js
+++ my-app/app/api/csc309-e11/route.js
@@ -2,7 +2,7 @@
 export async function GET() {
   return new Response(
     `<div id="auto-tester">
-    Hi. I am __utorid__, and I have learned how to develop a full-stack web app
+    Hi. I am hadzika1, and I have learned how to develop a full-stack web app
     and deploy it!
     </div>`,
     { headers: { "Content-Type": "text/html" } }
